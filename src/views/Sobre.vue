21593d3dd647091714d0a4eed9a4fa35e16389e8
LABD142$ <LABD142$@laboratorio.ifms.br>
2023-06-26 14:46:59 -0400

    New

--- src/views/Sobre.vue
+++ src/views/Sobre.vue
@@ -1,6 +1,11 @@
 <template>
     <div id="sobre">
             
+            <h1>
+                <img class="logo" src="/img/logo-Marvel.webp" alt="logo" width="200px">
+            </h1>
+
+            <hr>
 
             <div class="container">
           
@@ -12,9 +17,38 @@
                             <img :src="personagem.thumbnail.path+'.'+ personagem.thumbnail.extension" class="card-img-top" alt="...">
                             <div class="card-body">
                                 <h5 class="card-title">{{personagem.name}}</h5>
-                                <p class="card-text">
-                                    {{personagem.description}}
-                                </p>
+                                <div class="card-text">
+
+                                    <!-- Button trigger modal -->
+                                    <button type="button" class="btn btn-outline-primary " data-bs-toggle="modal" :data-bs-target="'#modal_'+personagem.id">
+                                    Descrição
+                                    </button>
+
+                                    <!-- Modal -->
+                                    <div class="modal fade" :id="'modal_'+personagem.id" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                                    <div class="modal-dialog">
+                                        <div class="modal-content">
+                                        <div class="modal-header">
+                                            <h5 class="modal-title" id="exampleModalLabel">Descrição</h5>
+                                            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                                        </div>
+                                        <div class="modal-body">
+                                            
+                                           <p class="card-text">
+                                                 {{personagem.description}}
+                                           </p> 
+
+                                        </div>
+                                        <div class="modal-footer">
+                                            <button type="button" class="btn btn-danger" data-bs-dismiss="modal">Close</button>
+                                        </div>
+                                        </div>
+                                    </div>
+                                    
+                                    </div>
+                               
+                                    
+                                </div>
                             </div>
                             </div>
                     </span>                      
